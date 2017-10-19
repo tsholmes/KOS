087732b6df95a19b8e4dce0f5037f7acc3ae1488
Tom Holmes <tholmes150@gmail.com>
2017-10-18 19:05:27 -0700

    add test asdf

--- src/kOS.Safe.Test/KS/ParserTest.cs
+++ src/kOS.Safe.Test/KS/ParserTest.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using kOS.Safe.Compilation.KS;
 using NUnit.Framework;
 
@@ -26,6 +27,33 @@ namespace kOS.Safe.Test.KS
         {
             parser.Parse("012345678901234567890123456789");
             parser.Parse("// 012345678901234567890123456789");
+        }
+        
+        [Test]
+        public void ParsedAllTestFiles() {
+          List<string> contents = new List<string>();
+          
+          // string baseDir = "../../../../kerboscript_tests";
+          string baseDir = "../../../../../../dunbaratu/kerboscripts";
+          
+          foreach(string file in Directory.GetFiles(baseDir, "*.ks", SearchOption.AllDirectories)) {
+            if (file.EndsWith("dock_passive_target.ks")) continue;
+            contents.Add(File.ReadAllText(file));
+          }
+          
+          Stopwatch stopwatch = Stopwatch.StartNew();
+          
+          foreach (string script in contents) {
+            ParseTree tree = parser.Parse(script);
+            if (tree.Errors.Count > 0) {
+              Console.WriteLine(script);
+              throw new Exception(tree.Errors[0].Message);
+            }
+          }
+          
+          stopwatch.Stop();
+          
+          Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
     }
 }
